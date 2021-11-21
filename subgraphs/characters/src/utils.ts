c27a56362e39875fab0f3fae969f4f061b962c6c
Bianca <inktense12@gmail.com>
2021-11-21 16:57:05 +0200

    Added SWAPI

---
+++ subgraphs/characters/src/utils.ts
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+export const getData = async <T>(id?: string): Promise<T | undefined> => {
+    try {
+        const result = await axios({
+          method: 'get',
+         url: `https://swapi.dev/api/people/` + ((id) ? `${id}/` : "")
+        });
+        return result.data;
+      } catch (error) {
+        console.error(`Error making SWAPI request: ${error}`);
+      }
+}
