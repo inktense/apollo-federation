552a59bc1d61cc26d3cc30022cdef5ba955899e4
Bianca <inktense12@gmail.com>
2022-01-15 13:27:53 +0200

    Added serverless to gateway to deploy as Lambda

--- gateway/gateway.ts
+++ gateway/gateway.ts
@@ -1,22 +1,36 @@
 import { APIGatewayProxyHandler, Context, Callback } from "aws-lambda";
-import { ApolloServer } from "apollo-server-lambda"
-import { ApolloGateway } from "@apollo/gateway"
+import { ApolloServer } from "apollo-server-lambda";
+import { ApolloGateway } from "@apollo/gateway";
 
 require("dotenv").config();
 
-export const gateway: APIGatewayProxyHandler = (_event, _context, callback) => {
+//export const gateway: APIGatewayProxyHandler = (_event, _context, callback) => {
   const gateway = new ApolloGateway();
 
-  const server = new ApolloServer({
-    gateway,
-    context: ({ event, context, express }) => ({
-      headers: event.headers,
-      functionName: context.functionName,
-      event,
-      context,
-      expressRequest: express.req,
-    }),
-  })
+//   const server = new ApolloServer({
+//     gateway,
+//     context: ({ event, context, express }) => ({
+//       headers: event.headers,
+//       functionName: context.functionName,
+//       event,
+//       context,
+//       expressRequest: express.req,
+//     }),
+//   });
 
-  server.createHandler();
-}
+//   server.createHandler();
+// };
+
+const server = new ApolloServer({
+  gateway,
+  context: ({ event, context, express }) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    event,
+    context,
+    expressRequest: express.req,
+  }),
+});
+
+exports.graphqlHandler = server.createHandler();
+//}
